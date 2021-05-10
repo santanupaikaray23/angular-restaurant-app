59a318d771c806b71fed8dd253722ff4902de5a9
kumar <santanupaikaray23@gmail.com>
2021-05-11 00:52:53 +0530

    add all changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,39 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { from } from 'rxjs';
+import {AddRestaurantComponent} from './add-restaurant/add-restaurant.component';
+import {ListRestaurantComponent} from './list-restaurant/list-restaurant.component'
+import {LoginComponent} from './login/login.component';
+import {RegisterComponent} from './register/register.component';
+import {UpdateRestaurantComponent} from './update-restaurant/update-restaurant.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    component:AddRestaurantComponent,
+    path:'add'
+  },
+  {
+    component:ListRestaurantComponent,
+    path:'list'
+  },
+  {
+    component:LoginComponent,
+    path:'login'
+
+  },
+  {
+    component:RegisterComponent,
+    path:'register'
+
+  },
+  {
+    component:UpdateRestaurantComponent,
+    path:'update'
+
+  }
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
